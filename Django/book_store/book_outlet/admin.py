0023d82f39118d84d68a59c8c56615fb56258994
matheus-dr <m47heusdr@protonmail.com>
2023-01-03 11:29:49 -0300

    feat: working with relationships in Django

--- Django/book_store/book_outlet/admin.py
+++ Django/book_store/book_outlet/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
 
-from . import models
+from .models import Book, Author, Address, Country
 
 
 class BookAdmin(admin.ModelAdmin):
+    list_filter = ('author', 'rating')
+    list_display = ('title', 'author')
     prepopulated_fields = {
         'slug': ('title',)
     }
 
 
-admin.site.register(models.Book, BookAdmin)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+class AddressAdmin(admin.ModelAdmin):
+    pass
+
+
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Country, CountryAdmin)
