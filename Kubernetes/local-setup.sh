1ec94a628daa80bebb057786bdbe301af406570b
matheus-dr <m47heusdr@protonmail.com>
2023-03-19 21:32:33 -0300

    first k8s commit :)

---
+++ Kubernetes/local-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+## Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+## Clean up
+rm -rf minikube-linux-amd64 kubectl
+
+## Start local cluster
+minikube start --driver=virtualbox
